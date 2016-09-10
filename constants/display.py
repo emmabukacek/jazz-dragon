09689786d861be679a2b8e709746e5cc88ff8960
Emma Bukacek <emma.bukacek@gmail.com>
2016-09-10 12:22:22 -0500

    Remove indentation and add jazz dragon logo to display constants

--- constants/display.py
+++ constants/display.py
@@ -1,8 +1,35 @@
+JAZZ_DRAGON_LOGO = """
+      _                 ____
+     | | __ _ ________ |  _ \ _ __ __ _  __ _  ___  _ __
+  _  | |/ _` |_  /_  / | | | | '__/ _` |/ _` |/ _ \| '_ \\
+ | |_| | (_| |/ / / /  | |_| | | | (_| | (_| | (_) | | | |
+  \___/ \__,_/___/___| |____/|_|  \__,_|\__, |\___/|_| |_|
+                                        |___/
+"""
+
+
+SETUP_MEDIUM_INTRO = """
+It looks like you haven't set up your Medium configuration yet.
+
+In order to use Jazz Dragon, you will need to create an
+application on your Medium account.
+
+Once your Medium application has been created, you can edit
+your configuration object to match your application values.
+"""
+
+
 SETUP_MEDIUM_PROMPT = """
-    It looks like you haven't set up your Medium configuration yet.
+Select an option:
+"""
 
-    In order to use Jazz Dragon, you will need to have created an
-    application on your Medium account.
+SETUP_MEDIUM_OPTIONS = {
+    '1': 'Create new application on Medium in your default browser',
+    '2': 'Edit your configuration object',
+    '3': 'Return to previous menu',
+    '4': 'Exit Jazz Dragon'
+}
 
-    Press any key to open settings in your editor.
-    """
+SETUP_MEDIUM_INVALID_OPTION_SELECTED = """
+Incorrect option. Try again.
+"""
