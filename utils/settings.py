50f77c486f3e1ba66543b9384774f16112850e21
Emma Bukacek <emma.bukacek@gmail.com>
2016-09-11 16:07:08 -0500

    Alphabetize methods in settings and remove return in edit

--- utils/settings.py
+++ utils/settings.py
@@ -4,6 +4,13 @@ import os.path
 from constants import configs
 
 
+def config_exists():
+    """
+    Check to see if settings exist.
+    """
+    return os.path.isfile(get_config_path())
+
+
 def create():
     """
     Creates initial Jazz Dragon settings.
@@ -19,7 +26,7 @@ def edit():
     if not config_exists():
         create()
 
-    return click.edit(filename=get_config_path())
+    click.edit(filename=get_config_path())
 
 
 def get_config_path():
@@ -29,10 +36,3 @@ def get_config_path():
     path = '{0}/{1}'.format(os.path.expanduser('~'),
                             configs.SETTINGS_FILE_NAME)
     return os.path.normpath(path)
-
-
-def config_exists():
-    """
-    Check to see if settings exist.
-    """
-    return os.path.isfile(get_config_path())
